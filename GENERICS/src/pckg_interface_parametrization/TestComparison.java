30fc5d46329ac24eaa40cc32645ba27b3e7f1c7b
Mndric <marija.andric379@gmail.com>
2025-10-15 17:37:04 +0200

    interfaces with parameters prvi dio predavanja

---
+++ GENERICS/src/pckg_interface_parametrization/TestComparison.java
@@ -0,0 +1,18 @@
+package pckg_interface_parametrization;
+
+public class TestComparison {
+    public static void main(String[] args) {
+        String sFst = "Tamo neki string...";
+        String sSnd = "Novi string koji je puno dug i nema kraja...";
+        CompareTWO<String> stringCompareTWO = new CompareTWO<>(sFst, sSnd);
+        stringCompareTWO.performComparison();
+        Integer iFst = 100;
+        Integer iSnd = 100;
+        CompareTWO<Integer> integerCompareTWO = new CompareTWO<>(iFst, iSnd);
+        integerCompareTWO.performComparison();
+        StringLengthComparison fst = new StringLengthComparison(sFst);
+        StringLengthComparison snd = new StringLengthComparison(sSnd);
+        CompareTWO<StringLengthComparison> stringLengthComparisonCompareTwo = new CompareTWO<>(fst, snd);
+        stringLengthComparisonCompareTwo.performComparison();
+    }
+}
